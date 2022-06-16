65d4b3f5aae13aa03600e5bd052ed7e630e20223
Sivathanu S <sivathanu1420@gmail.com>
2022-06-16 17:07:09 +0530

    For loop

--- src/Automation/LaunchBrowser.java
+++ src/Automation/LaunchBrowser.java
@@ -1,4 +1,4 @@
-package Utility;
+package Automation;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
---
+++ src/Java/ForLoop.java
@@ -0,0 +1,19 @@
+package Java;
+
+public class ForLoop {
+public static void main(String[] args) {
+		
+		for(int i=0;i>=0;i++)
+		{
+			if (i==100)
+					{
+						break;
+					}
+			
+			System.out.println("Sivathanu");
+		}
+		
+		
+		
+	}
+}
