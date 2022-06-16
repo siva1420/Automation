4c48640d3b1ce7bb879a45631ee733b24e8e919c
Sivathanu S <sivathanu1420@gmail.com>
2022-06-16 12:31:19 +0530

    code added

--- src/Utility/LaunchBrowser.java
+++ src/Utility/LaunchBrowser.java
@@ -1,5 +1,25 @@
 package Utility;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.gargoylesoftware.htmlunit.javascript.host.Set;
+
 public class LaunchBrowser {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\grid\\chromedriver.exe");
+		
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("http://www.leafground.com/");
+		
+		driver.findElement(By.xpath("//*[@id=\'post-153\']/div[2]/div/ul/li[1]/a/img")).click();
+		
+		
+
+	}
 
 }
