80e44c38d8e589d2bb5e3d8ae6f54af5156234ad
Sivathanu S <sivathanu1420@gmail.com>
2022-06-21 16:19:50 +0530

    Create DownloadaFile.java

---
+++ src/Automation/DownloadaFile.java
@@ -0,0 +1,22 @@
+package Automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DownloadaFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\grid\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/download.html");
+		
+		WebElement Downloadexcelfile=driver.findElement(By.linkText("Download Excel"));
+		Downloadexcelfile.click();
+		
+	}
+
+}
