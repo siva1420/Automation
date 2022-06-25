cf456ae7b80594a527a5f6cf46089a69ff9a31dd
Sivathanu S <sivathanu1420@gmail.com>
2022-06-25 13:31:05 +0530

    Create ClassTwo.java

---
+++ src/Java/ClassTwo.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class ClassTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassOne.firstmethod();
+		ClassOne.secondmethod();
+		ClassOne.Thirdmethod();
+		ClassOne.fourthmethod();
+		ClassOne.fifthmethod();
+
+	}
+
+}
