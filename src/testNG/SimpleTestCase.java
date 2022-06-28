3618b7d1587c95ef08d3a5bbfd6451a9a2827ab5
Sivathanu S <sivathanu1420@gmail.com>
2022-06-28 14:55:57 +0530

    Create SimpleTestCase.java

---
+++ src/testNG/SimpleTestCase.java
@@ -0,0 +1,32 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class SimpleTestCase {
+	
+	@Test
+	public void Firsttestcase() {
+		System.out.println("This is First Test Case");
+	}
+	
+	@Test
+	public void Secondtestcase() {
+		System.out.println("This is Second Test Case");
+	}
+	
+	@Test
+	public void Thirdtestcase() {
+		System.out.println("This is Third Test Case");
+	}
+	
+	@Test
+	public void Fourthtestcase() {
+		System.out.println("This is Fourth Test Case");
+	}
+	
+	@Test
+	public void Fifthtestcase() {
+		System.out.println("This is Fifth Test Case");
+	}
+
+}
