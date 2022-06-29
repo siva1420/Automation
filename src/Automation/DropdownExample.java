ae75c4547864ee49e4728d79452366fedcbb29e3
Sivathanu S <sivathanu1420@gmail.com>
2022-06-29 18:58:23 +0530

    Update DropdownExample.java

--- src/Automation/DropdownExample.java
+++ src/Automation/DropdownExample.java
@@ -23,20 +23,15 @@ public class DropdownExample {
 	/*	WebElement dropdown1= driver.findElement(By.id("dropdown1"));
 		Select select= new Select(dropdown1);
 		select.selectByIndex(2);
-*/		
-		WebElement dropdown2= driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[2]/select"));
-		Select text=new Select(dropdown2);
-	    Select.
+*/	 
 	    
 	    
 		driver.findElement(By.id("dropdown3"));
 		//Select Value= new Select(dropdown3);
-		select.selectByValue("2");
+		
 		
 		driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[4]/select"));
-		List<WebElement> listOfOption=select.getOptions();
-		int size=listOfOption.size();
-		System.out.println("Number of element is :"+size);
+
 		driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[5]/select"));
 	
 		driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[6]/select/option[1]"));
